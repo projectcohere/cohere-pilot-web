e91ede9fb373a245e56212620179219bc2828584
Ty Cobb <ty.cobb.m@gmail.com>
2019-10-16 14:07:47 -0400

    show errors on fields; relax ruby version

--- app/web/base/application_form_builder.rb
+++ app/web/base/application_form_builder.rb
@@ -1,17 +1,43 @@
 class ApplicationFormBuilder < ::ActionView::Helpers::FormBuilder
   def fieldset(title, *args, **kwargs, &children)
     a_class = kwargs.delete(:class)
-    @template.tag.div(*args, class: "#{a_class} Form-fieldset", **kwargs) do
+
+    # render the children & tag
+    f_class = "#{a_class} Form-fieldset"
+    content = @template.capture(&children)
+
+    @template.tag.div(*args, class: f_class, **kwargs) do
       @template.tag.h2(title) +
-      children.()
+      content
     end
   end
 
   def field(name, title = nil, *args, **kwargs, &children)
     a_class = kwargs.delete(:class)
-    label(name, *args, class: "#{a_class} Field", **kwargs) do
-      @template.tag.p(title || name.to_s.titlecase, class: "Field-hint") +
-      children.(name)
+
+    # check for field errors
+    has_errors =
+      object.respond_to?(:errors) &&
+      object.errors.is_a?(ActiveModel::Errors) &&
+      object.errors[name].present?
+
+    errors = nil
+    if has_errors
+      errors = object.errors[name].join(",")
+    end
+
+    # render the children & tag
+    f_class = "#{a_class} Field #{has_errors ? "is-error" : ""}"
+    content = @template.capture(&children)
+
+    label(name, *args, class: f_class, **kwargs) do
+      hint = @template.tag.p(class: "Field-hint") do
+        hint_text = @template.tag.span(title || name.to_s.titlecase)
+        hint_error = has_errors ? @template.tag.span(" #{errors}", class: "Field-errors") : ""
+        hint_text + hint_error
+      end
+
+      hint + content
     end
   end
 end
