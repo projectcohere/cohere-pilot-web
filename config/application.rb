af177d870b1f51ad7d527fc6c5bf31441b678e57
Ty Cobb <ty.cobb.m@gmail.com>
2019-10-21 17:33:35 -0400

    add custom fonts

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,12 @@ Bundler.require(*Rails.groups)
 module CoherePilotWeb
   class Application < Rails::Application
     config.load_defaults(6.0)
+
+    # assets
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # autoload
     config.autoload_paths += %w[
       app/features/base
       app/web/base
