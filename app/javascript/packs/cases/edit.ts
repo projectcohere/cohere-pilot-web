893d4513c2a30344250c29e27f9eb37bda797507
Ty Cobb <ty.cobb.m@gmail.com>
2019-10-21 14:32:29 -0400

    add delete links to income rows

--- app/javascript/packs/cases/edit.ts
+++ app/javascript/packs/cases/edit.ts
@@ -2,9 +2,9 @@
 class ShowEdit {
   // -- props --
   // -- props/el
-  private $addIncomeRows: HTMLElement
-  private $addIncomeRow: HTMLElement
-  private $addIncomeLink: HTMLElement
+  private $incomeRows: Element
+  private $incomeRowTemplate: Element
+  private $addIncomeLink: Element
 
   // -- lifecycle --
   static start() {
@@ -15,11 +15,13 @@ class ShowEdit {
   private onMount() {
     const $c = this
 
-    $c.$addIncomeRows = $c.getById("add-income-rows")
-    $c.$addIncomeRow = $c.getById("add-income-row").cloneNode(true) as HTMLElement
+    $c.$incomeRowTemplate = $c.createTemplateFromIncomeRow($c.getById("income-row-template"))
+
+    $c.$incomeRows = $c.getById("income-rows")
+    $c.$incomeRows.addEventListener("click", $c.didClickDeleteIncomeLink)
 
     $c.$addIncomeLink = $c.getById("add-income-link")
-    $c.$addIncomeLink.onclick = $c.didClickAddIncomeLink
+    $c.$addIncomeLink.addEventListener("click", $c.didClickAddIncomeLink)
   }
 
   // -- events --
@@ -28,28 +30,47 @@ class ShowEdit {
     $c.addIncomeRow()
   }
 
+  private didClickDeleteIncomeLink = (event: MouseEvent) => {
+    const $c = this
+
+    // filter to clicks on a delete link
+    const incomeRowDeleteLink = event.target as Element
+    if ($c.isIncomeRowDeleteLink(incomeRowDeleteLink)) {
+      $c.removeIncomeRow(incomeRowDeleteLink.parentElement)
+    }
+  }
+
   // -- commands --
   private addIncomeRow() {
     const $c = this
 
     // create a new row from the template
-    const row = $c.$addIncomeRow.cloneNode(true) as HTMLElement
+    const row = $c.$incomeRowTemplate.cloneNode(true) as Element
 
-    // clean inputs and assign the right index
-    const index = $c.$addIncomeRows.children.length
-    const inputs = Array.from(row.getElementsByTagName("input"))
-
-    for (const input of inputs) {
-      input.value = ""
-      $c.setIndexOfInput(input, index)
-    }
+    // assign the right index
+    const index = $c.$incomeRows.children.length
+    $c.setIndexOfIncomeRow(row, index)
 
     // add the row to the list
-    $c.$addIncomeRows.appendChild(row)
+    $c.$incomeRows.appendChild(row)
+  }
+
+  private removeIncomeRow(incomeRow: Element) {
+    const $c = this
+
+    // remove the matching income row
+    $c.$incomeRows.removeChild(incomeRow)
+
+    // re-assign the row indices
+    let index = 0
+    for (const incomeRow of Array.from($c.$incomeRows.children)) {
+      $c.setIndexOfIncomeRow(incomeRow, index)
+      index++
+    }
   }
 
   // -- queries --
-  private getById(id: string): HTMLElement {
+  private getById(id: string): Element {
     const el = document.getElementById(id)
     if (el == null) {
       throw `Failed to find element with id ${id}!`
@@ -60,9 +81,32 @@ class ShowEdit {
   }
 
   // -- el --
-  private setIndexOfInput(input: HTMLInputElement, index: number) {
-    input.setAttribute("id", input.id.replace(/\d+/, index.toString()))
-    input.setAttribute("name", input.name.replace(/\d+/, index.toString()))
+  // -- el/income-row
+  private getInputsFromIncomeRow(row: Element): HTMLInputElement[] {
+    return Array.from(row.getElementsByTagName("input"))
+  }
+
+  private setIndexOfIncomeRow(row: Element, index: number) {
+    const $c = this
+    for (const input of $c.getInputsFromIncomeRow(row)) {
+      input.setAttribute("id", input.id.replace(/\d+/, index.toString()))
+      input.setAttribute("name", input.name.replace(/\d+/, index.toString()))
+    }
+  }
+
+  private createTemplateFromIncomeRow(source: Element): Element {
+    const $c = this
+
+    const template = source.cloneNode(true) as Element
+    for (const input of $c.getInputsFromIncomeRow(template)) {
+      input.value = ""
+    }
+
+    return template
+  }
+
+  private isIncomeRowDeleteLink(element: Element | null): boolean {
+    return element != null && element.className.includes("CaseForm-incomeDelete")
   }
 }
 
