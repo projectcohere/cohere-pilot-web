fb40368e3227755c1ba1ae880295427d58342ff2
Ty Cobb <ty.cobb.m@gmail.com>
2019-10-23 10:54:30 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,44 @@
 # -- organizations --
-enroller = Enroller::Record.create!(
+supplier_0 = Supplier::Record.create!(
+  name: "DTE"
+)
+
+supplier_1 = Supplier::Record.create!(
+  name: "Consumers Energy"
+)
+
+enroller_0 = Enroller::Record.create!(
   name: "Wayne Metro"
 )
 
 # -- users --
 # TODO: add a diceware password generator to create accounts for everyone
 User::Record.create!(
-  email: "ty@civilla.com",
+  email: "me@cohere.com",
   password: "password",
   organization_type: :cohere
 )
 
 User::Record.create!(
-  email: "shama@waynemetro.com",
+  email: "me@dteenergy.com",
   password: "password",
-  organization: enroller
+  organization: supplier_0
+)
+
+User::Record.create!(
+  email: "me@consumersenergy.com",
+  password: "password",
+  organization: supplier_1
+)
+
+User::Record.create!(
+  email: "me@michigan.gov",
+  password: "password",
+  organization_type: :dhs
+)
+
+User::Record.create!(
+  email: "me@waynemetro.org",
+  password: "password",
+  organization: enroller_0
 )
