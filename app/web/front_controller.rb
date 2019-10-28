31108bf6bab19e8d71ef1fe37e74ed18a6cc6935
Ty Cobb <ty.cobb.m@gmail.com>
2019-10-28 11:48:56 -0400

    disable csrf protection on webhook, log headers

--- app/web/front_controller.rb
+++ app/web/front_controller.rb
@@ -1,4 +1,9 @@
 class FrontController < ApplicationController
+  protect_from_forgery(except: [:messages])
+
   def messages
+    request.headers.each do |(key, value)|
+      logger.debug("#{key}=#{value}")
+    end
   end
 end
