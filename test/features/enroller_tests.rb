8202eb3945136f75a56fa347eae6b57b9ebfcc48
Ty Cobb <ty.cobb.m@gmail.com>
2019-10-28 14:38:46 -0400

    clean up test names

--- test/features/enroller_tests.rb
+++ test/features/enroller_tests.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class EnrollerTests < ActiveSupport::TestCase
-  test "an enroller can be constructed from a record" do
+  test "can be constructed from a record" do
     enroller = Enroller.from_record(enrollers(:enroller_1))
     assert_not_nil(enroller.id)
   end
--- test/features/user_tests.rb
+++ test/features/user_tests.rb
@@ -1,24 +1,24 @@
 require "test_helper"
 
 class UserTests < ActiveSupport::TestCase
-  test "an operator can be constructed from a record" do
+  test "can be constructed from a operator record" do
     user = User.from_record(users(:cohere_1))
     assert_equal(user.role, :cohere)
   end
 
-  test "an enroller can be constructed from a record" do
+  test "can be constructed from an enroller record" do
     user = User.from_record(users(:enroller_1))
     assert_equal(user.role, :enroller)
     assert_not_nil(user.organization)
   end
 
-  test "a supplier can be constructed from a record" do
+  test "can be constructed from a supplier record" do
     user = User.from_record(users(:supplier_1))
     assert_equal(user.role, :supplier)
     assert_not_nil(user.organization)
   end
 
-  test "a dhs partner can be constructed from a record" do
+  test "can be constructed from a dhs partner record" do
     user = User.from_record(users(:dhs_1))
     assert_equal(user.role, :dhs)
   end
