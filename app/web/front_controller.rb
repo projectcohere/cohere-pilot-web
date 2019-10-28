89d7ab1ad7b243f694c5ee554ea098e6d9047dc7
Ty Cobb <ty.cobb.m@gmail.com>
2019-10-28 11:51:07 -0400

    also log raw post body

--- app/web/front_controller.rb
+++ app/web/front_controller.rb
@@ -2,8 +2,14 @@ class FrontController < ApplicationController
   protect_from_forgery(except: [:messages])
 
   def messages
+    logger.debug("POST /front/messages")
+
+    logger.debug("headers\n-------")
     request.headers.each do |(key, value)|
       logger.debug("#{key}=#{value}")
     end
+
+    logger.debug("body\n----")
+    logger.debug(request.raw_post)
   end
 end
