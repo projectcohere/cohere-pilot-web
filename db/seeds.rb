7d6a53980829e367f03c4c856f76eaebacd516b3
Ty Cobb <ty.cobb.m@gmail.com>
2019-11-03 22:32:40 -0500

    remove users from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,35 +10,3 @@ supplier_1 = Supplier::Record.create!(
 enroller_0 = Enroller::Record.create!(
   name: "Wayne Metro"
 )
-
-# -- users --
-# TODO: add a diceware password generator to create accounts for everyone
-User::Record.create!(
-  email: "me@cohere.org",
-  password: "password",
-  organization_type: :cohere
-)
-
-User::Record.create!(
-  email: "me@dteenergy.com",
-  password: "password",
-  organization: supplier_0
-)
-
-User::Record.create!(
-  email: "me@consumersenergy.com",
-  password: "password",
-  organization: supplier_1
-)
-
-User::Record.create!(
-  email: "me@michigan.gov",
-  password: "password",
-  organization_type: :dhs
-)
-
-User::Record.create!(
-  email: "me@waynemetro.org",
-  password: "password",
-  organization: enroller_0
-)
