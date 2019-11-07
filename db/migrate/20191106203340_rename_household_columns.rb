765f293f074131596cfc587232a926d59157121b
Ty Cobb <ty.cobb.m@gmail.com>
2019-11-07 16:30:29 -0500

    fix migration that got clobbered in a rename

--- db/migrate/20191106203340_rename_household_columns.rb
+++ db/migrate/20191106203340_rename_household_columns.rb
@@ -1,8 +1,8 @@
 class RenameHouseholdColumns < ActiveRecord::Migration[6.0]
   def change
     change_table(:recipients) do |t|
-      t.rename(:household_size, :household_size)
-      t.rename(:household_income, :household_income)
+      t.rename(:dhs_household_size, :household_size)
+      t.rename(:dhs_household_income, :household_income)
     end
   end
 end
