efd097da86618b1d0c989a972eddb0afb756952e
Ty Cobb <ty.cobb.m@gmail.com>
2019-11-06 23:54:50 -0500

    use a single mailer for cases

--- app/web/cases/supplier_controller.rb
+++ app/web/cases/supplier_controller.rb
@@ -43,7 +43,7 @@ module Cases
 
       note = Case::Notes::OpenedCase::Broadcast.new
       note.receiver_ids.each do |receiver_id|
-        SupplierMailer.opened_case(@form.case_id, receiver_id).deliver_later
+        CasesMailer.opened_case(@form.case_id, receiver_id).deliver_later
       end
 
       redirect_to(cases_supplier_index_path, notice: "Created case!")
--- app/web/cases_mailer.rb
+++ app/web/cases_mailer.rb
@@ -3,6 +3,15 @@ class CasesMailer < ApplicationMailer
   helper_method(:case_scope)
 
   # -- actions --
+  def opened_case(case_id, user_id)
+    @note = Case::Notes::OpenedCase.new(case_id, user_id)
+
+    mail(
+      subject: @note.title,
+      to: @note.receiver_email
+    )
+  end
+
   def submitted_case(case_id, user_id)
     @note = Case::Notes::SubmittedCase.new(case_id, user_id)
 
