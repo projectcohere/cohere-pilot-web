401b2d682601391397238059e4d6404d2b32c6da
Ty Cobb <ty.cobb.m@gmail.com>
2019-11-14 14:36:21 -0500

    pdf css yikes

--- test/functional/cases_tests.rb
+++ test/functional/cases_tests.rb
@@ -230,8 +230,8 @@ class CasesTests < ActionDispatch::IntegrationTest
     assert_enqueued_jobs(1)
 
     pdf_text = text_from_pdf_file(case_rec.documents[0].file)
-    assert_match(/MEAP Agreement/, pdf_text)
-    assert_match(/Danice Sample/, pdf_text)
+    assert_match(/MEAP\s*Agreement/, pdf_text)
+    assert_match(/Danice\s*Sample/, pdf_text)
   end
 
   test "show errors for an invalid case" do
