27739253e050994b16b017cc5bfaff4d818cc04a
Ty Cobb <ty.cobb.m@gmail.com>
2019-11-18 13:49:04 -0500

    force ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local = false
+  config.force_ssl = true
 
   # -- root/logging
   config.log_level = :debug
