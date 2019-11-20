81f839b4baf0124dc8ab6efab8ec58230fb5ae21
Ty Cobb <ty.cobb.m@gmail.com>
2019-11-20 08:15:29 -0500

    realizing now that prod pdf margins were broken because options were overwritten

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,18 +1,15 @@
 PDFKit.configure do |config|
-  # workarounds for rendering differences on linux/heroku
-  # see: https://github.com/wkhtmltopdf/wkhtmltopdf/issues/2171
-  page_zoom = Rails.env.production? ? 0.75 : 1
-  page_margin = "#{0.75 / page_zoom}in"
-
   config.default_options = {
     quiet: true,
     disable_smart_shrinking: false,
     page_size: "Letter",
     encoding: "UTF-8",
-    zoom: page_zoom,
-    margin_top: page_margin,
-    margin_bottom: page_margin,
-    margin_left: page_margin,
-    margin_right: page_margin
+    margin_top: "0.75in",
+    margin_bottom: "0.75in",
+    margin_left: "0.75in",
+    margin_right: "0.75in",
+    # workarounds for rendering differences on linux/heroku
+    # see: https://github.com/wkhtmltopdf/wkhtmltopdf/issues/2171
+    zoom: Rails.env.production? ? 0.75 : 1
   }
 end
