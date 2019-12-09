eb4d8afa326408a49c29f2059e8ed8675e39cf0b
Ty Cobb <ty.cobb.m@gmail.com>
2019-12-09 11:03:12 -0600

    fix spelling mistake

--- db/migrate/20191209161510_make_wrap_fields_nonnull.rb
+++ db/migrate/20191209161510_make_wrap_fields_nonnull.rb
@@ -1,4 +1,4 @@
-class MakeWrapFielsNonnull < ActiveRecord::Migration[6.0]
+class MakeWrapFieldsNonnull < ActiveRecord::Migration[6.0]
   def change
     change_column_null(:cases, :supplier_account_active_service, false)
     change_column_null(:recipients, :household_ownership, false)
