b0d343d90f3cef9268ba24ac35cb6dece49348f8
Ty Cobb <ty.cobb.m@gmail.com>
2019-12-16 14:51:10 -0600

    add error dot form section filters

--- app/web/shared/application_helper.rb
+++ app/web/shared/application_helper.rb
@@ -1,6 +1,29 @@
 module ApplicationHelper
-  private def cx(*classes, states)
-    classes += states.is_a?(Hash) ? states.filter { |_, v| v }.keys : states
-    classes.join(" ")
+  # -- elements --
+  def filter_for(id, is_selected: false, is_error: false)
+    link_to(id.to_s.titlecase, "##{id}",
+      class: cx("Filter", is_selected: is_selected, is_error: is_error),
+      data: { turbolinks: false }
+    )
+  end
+
+  # -- queries --
+  def errors?(*models)
+    models.any? do |model|
+      model.errors.present?
+    end
+  end
+
+  # -- utilities --
+  def cx(*classes, **states)
+    active_classes = classes.dup
+
+    states.each do |key, is_active|
+      if is_active
+        active_classes << key.to_s.dasherize
+      end
+    end
+
+    active_classes.join(" ")
   end
 end
