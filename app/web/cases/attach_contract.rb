b312917a6c4076ee8722e5c265668dd47a25df6a
Ty Cobb <ty.cobb.m@gmail.com>
2020-04-02 15:47:43 -0500

    remove front and dead code paths for cases

--- app/web/cases/attach_contract.rb
+++ app/web/cases/attach_contract.rb
@@ -1,9 +1,15 @@
 module Cases
   class AttachContract < ApplicationWorker
+    # -- lifetime --
+    def initialize(case_repo: Case::Repo.new)
+      @case_repo = case_repo
+    end
+
     # -- command --
     def call(case_id, document_id)
-      attach_file = AttachFile.new(generate: GenerateContract.new)
-      attach_file.(case_id, document_id)
+      @case = @case_repo.find_with_document(case_id, document_id)
+      @case.attach_file_to_selected_document(GenerateContract.(@case))
+      @case_repo.save_selected_attachment(@case)
     end
 
     alias :perform :call
--- test/support/pre/vcr.rb
+++ test/support/pre/vcr.rb
@@ -7,7 +7,6 @@ VCR.configure do |c|
     allow_unused_http_interactions: false
   }
 
-  c.filter_sensitive_data("<FRONT_API_JWT>") { ENV["FRONT_API_JWT"] }
   c.filter_sensitive_data("<TWILIO_API_ACCOUNT_SID>") { ENV["TWILIO_API_ACCOUNT_SID"] }
   c.filter_sensitive_data("<TWILIO_API_AUTH_TOKEN>") { ENV["TWILIO_API_AUTH_TOKEN"] }
   c.filter_sensitive_data('<TWILIO_API_BASIC_AUTH>') { Base64.strict_encode64("#{ENV.fetch("TWILIO_API_ACCOUNT_SID")}:#{ENV.fetch("TWILIO_API_AUTH_TOKEN")}") }
