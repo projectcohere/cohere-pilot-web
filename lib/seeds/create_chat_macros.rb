59cfb0ef7696e85a5abaa0d043689b42372b81f6
Ty Cobb <ty.cobb.m@gmail.com>
2020-05-08 10:17:17 -0500

    update macros; make seed file idempotent

--- lib/seeds/create_chat_macros.rb
+++ lib/seeds/create_chat_macros.rb
@@ -1,11 +1,22 @@
+# -- types --
+Macro = Struct.new(:path, :filename)
+
+# -- main --
 macros = Rails.root.join("macros")
   .children
   .filter { |p| p.extname == ".png" }
+  .map { |p| Macro.new(p, p.basename.to_s) }
 
-macros.each do |path|
-  ActiveStorage::Blob.create_and_upload!(
-    io: File.new(path),
-    filename: path.basename.to_s,
-    content_type: "image/png",
-  )
+created = ActiveStorage::Blob
+  .where(filename: [macros.map(&:filename)])
+  .pluck(:filename)
+
+macros.each do |m|
+  if not created.include?(m.filename)
+    ActiveStorage::Blob.create_and_upload!(
+      io: File.new(m.path),
+      filename: m.filename,
+      content_type: "image/png",
+    )
+  end
 end
