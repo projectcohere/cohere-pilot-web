1a28a91a8189cb671565ec085731fc12c6598944
Ty Cobb <ty.cobb.m@gmail.com>
2020-08-14 14:36:19 -0400

    demo constants

--- app/demo/build_demo_site.rb
+++ app/demo/build_demo_site.rb
@@ -1,14 +1,31 @@
 require "demo_ext"
 
 class BuildDemoSite < ::Command
+  # -- constants --
+  class Sheets < ::Option
+    option(:application)
+    option(:demo)
+  end
+
+  class Roles < ::Option
+    option(:applicant)
+  end
+
+  module Paths
+    # -- destinations
+    Demo = Pathname.new("./demo")
+    DemoStorage = Demo.join("storage")
+
+    # -- sources
+    Public = Pathname.new("./public")
+    Assets = Public.join("assets")
+    Macros = Pathname.new("./macros")
+  end
+
   # -- lifetime --
   def initialize(repo = DemoRepo.new, store = DemoStore.new)
     @repo = repo
     @store = store
-    @demo_dir ||= Pathname.new("./demo")
-    @demo_storage_dir ||= @demo_dir.join("storage")
-    @public_dir ||= Pathname.new("./public")
-    @macros_dir ||= Pathname.new("./macros")
   end
 
   # -- command --
@@ -21,7 +38,7 @@ class BuildDemoSite < ::Command
 
     # create pages
     create_page("index.html", render.landing)
-    create_page("applicant/1", render.a01_phone)
+    create_page("1", render.a01_phone, role: Roles::Applicant)
     create_page("1", render.s01_sign_in)
     create_page("2", render.s02_source_list)
     create_page("3", render.s03_source_start_case)
@@ -31,21 +48,19 @@ class BuildDemoSite < ::Command
   private def scaffold_dir
     # create clearn root dir (remove demo_dir's contents instead of the
     # dir itself so that demo-server doesn't error on rebuilt paths)
-    @demo_dir.glob("*").each(&:rmtree) if @demo_dir.exist?
-    @demo_dir.mkpath
+    Paths::Demo.glob("*").each(&:rmtree) if Paths::Demo.exist?
+    Paths::Demo.mkpath
 
     # symlink public files
-    @public_dir.children.each do |public_path|
-      src = public_path.relative_path_from(@demo_dir)
-      dst = @demo_dir.join(src.basename)
+    Paths::Public.children.each do |public_path|
+      src = public_path.relative_path_from(Paths::Demo)
+      dst = Paths::Demo.join(src.basename)
       dst.make_symlink(src)
     end
 
     # symlink namespaced css files
-    sheets = %w[application demo]
-
-    @public_dir.join("assets").children.each do |asset_path|
-      sheets.each do |name|
+    Paths::Assets.children.each do |asset_path|
+      Sheets.each do |name|
         if asset_path.basename.fnmatch("#{name}-*.css")
           src = asset_path.basename
           dst = asset_path.sub("#{name}-", "#{name}.self-")
@@ -55,22 +70,16 @@ class BuildDemoSite < ::Command
     end
 
     # create static storage dir to replace activestorage
-    @demo_storage_dir.mkpath
+    Paths::DemoStorage.mkpath
 
     # symlink macros into storage
-    @macros_dir.children.each do |macro_path|
+    Paths::Macros.children.each do |macro_path|
       if macro_path.extname == ".png"
-        src = macro_path.relative_path_from(@demo_storage_dir)
-        dst = @demo_storage_dir.join(src.basename)
+        src = macro_path.relative_path_from(Paths::DemoStorage)
+        dst = Paths::DemoStorage.join(src.basename)
         dst.make_symlink(src)
       end
     end
-  end
-
-  private def link_stylesheet(name, asset)
-    src = asset.basename
-    dst = asset.sub("#{name}-", "#{name}.self-")
-    dst.make_symlink(src) if not dst.exist?
   end
 
   private def mock_service(type, value)
@@ -78,8 +87,8 @@ class BuildDemoSite < ::Command
     Service::Container.attributes[name] = value
   end
 
-  private def create_page(name, html)
-    path = @demo_dir.join("#{name}")
+  private def create_page(name, html, role: "")
+    path = Paths::Demo.join(role.to_s, name)
     path.parent.mkpath
     path.write(html)
   end
